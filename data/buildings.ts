2eed898cc03fa64360160ffd0b09d00dee1d4b4f
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-05-25 19:56:10 +0800

    feat: add image URLs for buildings and include corresponding image files

--- data/buildings.ts
+++ data/buildings.ts
@@ -50,6 +50,7 @@ export const buildings: Building[] = [
     numberOfFloors: 3,
     function: "OFFICE AND SCHOOL",
     // buildingAge: 0,
+    imageUrl: "/images/buildings/DIET.jpg",
   },
   {
     id: "department-of-information-technology",
@@ -58,6 +59,7 @@ export const buildings: Building[] = [
     numberOfFloors: 5,
     function: "OFFICE AND SCHOOL",
     // buildingAge: 0,
+    imageUrl: "/images/buildings/DIT.jpg",
   },
   {
     id: "engineering-science-building",
