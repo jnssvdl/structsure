5de91953bbda3fc8cd3fb39f617845d87bc11a95
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2025-10-13 02:14:31 +0800

    feat: replace Poppins font with Roboto Mono in layout for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import Footer from "@/components/footer";
 
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+const poppins = Roboto_Mono({
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
